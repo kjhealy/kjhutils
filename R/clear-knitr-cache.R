6191eeeef6ee8ab0f3fa5178ba59bc4487d6128c
Kieran Healy <kjhealy@gmail.com>
2013-05-16 08:28:43 -0400

    Simple (and dangerous) clear knitr cache fn

---
+++ R/clear-knitr-cache.R
@@ -0,0 +1,13 @@
+##' Delete everything in the project "cache" directory
+##' Removed all files in "../cache" by default.
+##' @title Delete knitr cache
+##' @param cachedir The name of the directory in the present project
+##(via dirname(getwd())) where files are to be deleted.
+##' @return Deletes all files in the specified directory
+##' @author Kieran Healy
+clear.knitr.cache <- function(cachedir="cache") {
+  parent.dir <- dirname(getwd())
+  target.dir <- paste(parent.dir, "/", cachedir, sep="")
+  full.cmd <- paste("rm -f ",target.dir,"/*.*", sep="")
+  system(full.cmd)
+}
