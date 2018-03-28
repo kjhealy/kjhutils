5191974332e0be3d0d866b97e55166dc93611129
Kieran Healy <kjhealy@gmail.com>
2018-03-28 14:37:29 -0400

    Use black not grey in my.colors

--- R/palette.r
+++ R/palette.r
@@ -8,11 +8,11 @@
 ##' @author Kieran Healy
 ##' @export
 my.colors <- function(palette="cb"){
-  ### The palette with grey:
-  cb.palette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+### The palette with black:
+  cb.palette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
   ## Same one Reversed
   rcb.palette <- rev(cb.palette)
   ## Blue and yellow first choices
-  bly.palette <- c("#E69F00", "#0072B2", "#999999", "#56B4E9", "#009E73", "#F0E442", "#D55E00", "#CC79A7")
+  bly.palette <- c("#E69F00", "#0072B2", "#000000", "#56B4E9", "#009E73", "#F0E442", "#D55E00", "#CC79A7")
   if (palette=="cb") return(cb.palette) else if (palette=="rcb") return(rcb.palette) else if (palette=="bly") return(bly.palette) else stop("Choose cb, rcb, or bly ony.")
 }
